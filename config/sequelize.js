f8a1254a9992566583b87a213debf777eec1cc7f
romulodm <romulotg12@gmail.com>
2025-08-31 13:46:17 -0300

    feat: redis and sequelize configurations

---
+++ config/sequelize.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+const config = {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+};
+
+const connection = new Sequelize(config);
+module.exports = connection;
